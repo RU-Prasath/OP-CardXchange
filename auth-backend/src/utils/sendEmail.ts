025fa5e93194825b5c935fd99873cbc9e6442c14
RU-Prasath <prasathru14@gmail.com>
2025-12-10 13:01:31 +0530

    sendEmail changed from nodemailer to resend

--- auth-backend/src/utils/sendEmail.ts
+++ auth-backend/src/utils/sendEmail.ts
@@ -22,9 +22,12 @@
 // }
 
 import { Resend } from "resend";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
+console.log(process.env.RESEND_API_KEY)
 export async function sendEmail(to: string, subject: string, html: string) {
   try {
     const response = await resend.emails.send({
